143a39a71614539c68fee7ea19df368c333d8e93
koralle <mamoru.kosugiyama@ncdc.co.jp>
2020-06-18 07:53:34 +0900

    dd: file-loaderの導入

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -34,6 +34,14 @@ module.exports = {
         test: /\.(css|sass|scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|oft)$/,
+        loader: "file-loader",
+        options: {
+          limit: 8192,
+          name: path.resolve(__dirname, "images/[name].[ext]"),
+        },
+      },
     ],
   },
 
